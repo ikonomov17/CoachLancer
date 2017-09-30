ee67293bcc99c6dad50d1f7689767460654c5e2d
Aleksandar Ikonomov <aleksandar.ikonomov@abv.bg>
2017-09-30 21:50:30 +0300

    Extracted Data Layer. Moved DbContext to Data project and User to Data Models.

---
+++ CoachLancer/CoachLancer.Data/MsSqlDbContext.cs
@@ -0,0 +1,19 @@
+﻿using CoachLance.Data.Models;
+using Microsoft.AspNet.Identity.EntityFramework;
+
+namespace CoachLancer.Data
+{
+    // You can add profile data for the user by adding more properties to your ApplicationUser class, please visit https://go.microsoft.com/fwlink/?LinkID=317594 to learn more.
+    public class MsSqlDbContext : IdentityDbContext<User>
+    {
+        public MsSqlDbContext()
+            : base("DefaultConnection", throwIfV1Schema: false)
+        {
+        }
+
+        public static MsSqlDbContext Create()
+        {
+            return new MsSqlDbContext();
+        }
+    }
+}
