32f6f9f85cc752c95491f16e01e11ea15ac49217
Aleksandar Ikonomov <aleksandar.ikonomov@abv.bg>
2017-09-30 22:47:01 +0300

    Abstracted data models.

---
+++ CoachLancer/CoachLance.Data.Models/Abstracts/DataModel.cs
@@ -0,0 +1,30 @@
+﻿using CoachLance.Data.Models.Contracts;
+using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace CoachLance.Data.Models.Abstracts
+{
+    public abstract class DataModel : IDeletable, IAuditable
+    {
+        public DataModel()
+        {
+            this.Id = Guid.NewGuid();
+        }
+
+        [Key]
+        public Guid Id { get; set; }
+
+        [Index]
+        public bool IsDeleted { get; set; }
+
+        [DataType(DataType.DateTime)]
+        public DateTime? DeletedOn { get; set; }
+
+        [DataType(DataType.DateTime)]
+        public DateTime? CreatedOn { get; set; }
+
+        [DataType(DataType.DateTime)]
+        public DateTime? ModifiedOn { get; set; }
+    }
+}
