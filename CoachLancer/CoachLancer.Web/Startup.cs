86d6b15f6f505859af9d5a4a788f5ea16db15cfd
Aleksandar Ikonomov <aleksandar.ikonomov@abv.bg>
2017-09-30 13:53:57 +0300

    Added ASP.NET MVC project with Authentication. Added unit tests project

---
+++ CoachLancer/CoachLancer.Web/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(CoachLancer.Web.Startup))]
+namespace CoachLancer.Web
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
