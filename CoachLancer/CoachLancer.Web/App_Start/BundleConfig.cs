2cf5fb3f68e1e5ace5820218b1bc6d535c0d57dc
Aleksandar Ikonomov <aleksandar.ikonomov@abv.bg>
2017-10-04 21:15:23 +0300

    Customized navbar. Deleted index content

--- CoachLancer/CoachLancer.Web/App_Start/BundleConfig.cs
+++ CoachLancer/CoachLancer.Web/App_Start/BundleConfig.cs
@@ -25,7 +25,8 @@ namespace CoachLancer.Web
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
                       "~/Content/bootstrap.css",
-                      "~/Content/site.css"));
+                      "~/Content/site.css",
+                      "~/Content/styles.css"));
         }
     }
 }
